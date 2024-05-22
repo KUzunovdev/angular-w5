f96db3dbd84ba6271c61c559d1e612d4415e073a
Ki11akrokBg <kikouzunow@gmail.com>
2024-05-22 20:50:43 +0300

    feat: add routes for home, live market, historical data, and predictions components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LiveMarketComponent } from './live-market/live-market.component';
+import { HistoricalDataComponent } from './historical-data/historical-data.component';
+import { PredictionsComponent } from './predictions/predictions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'live-market', component: LiveMarketComponent },
+    { path: 'historical-data', component: HistoricalDataComponent },
+    { path: 'predictions', component: PredictionsComponent }
+];
---
+++ src/app/live-market/live-market.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-market',
+  standalone: true,
+  imports: [],
+  templateUrl: './live-market.component.html',
+  styleUrl: './live-market.component.scss'
+})
+export class LiveMarketComponent {
+
+}
